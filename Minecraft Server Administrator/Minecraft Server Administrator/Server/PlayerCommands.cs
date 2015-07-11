d365e33a847d705ea814186473726132aab2c380
conterio <jeremyconterio@gmail.com>
2015-07-11 17:02:00 -0600

    added player command class

---
+++ Minecraft Server Administrator/Minecraft Server Administrator/Server/PlayerCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Minecraft_Server_Administrator
+{
+    
+
+    class PlayerCommands
+    {
+        ConsoleControl.WPF.ConsoleControl console = MainWindowContent.instance.Console;
+
+        PlayerCommands(){
+
+
+            console.WriteInput("command", Colors.White, false);
+        }
+
+
+
+    }
+}
